f8e81ce6da6d1efd62cdb2d951f674bbf8f3b66d
GelminasP <83804343+GelminasP@users.noreply.github.com>
2021-09-02 10:59:47 +0100

    Minor Bug Fix 9

--- main.py
+++ main.py
@@ -70,6 +70,43 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(250), nullable=False)
     lists = relationship("List", back_populates="user")
 
+    __hash__ = object.__hash__
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return str(self.id)
+        except AttributeError:
+            raise NotImplementedError('No `id` attribute - override `get_id`')
+
+    def __eq__(self, other):
+        '''
+        Checks the equality of two `UserMixin` objects using `get_id`.
+        '''
+        if isinstance(other, UserMixin):
+            return self.get_id() == other.get_id()
+        return NotImplemented
+
+    def __ne__(self, other):
+        '''
+        Checks the inequality of two `UserMixin` objects using `get_id`.
+        '''
+        equal = self.__eq__(other)
+        if equal is NotImplemented:
+            return NotImplemented
+        return not equal
+
 
 # db.create_all()
 
